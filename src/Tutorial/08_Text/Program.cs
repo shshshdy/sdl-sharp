c9edc995b40adcba0c5b6d4f872d9f984501e920
ssd <shshshdy@163.com>
2024-04-23 15:23:20 +0800

    windows10 及以上  dx11

--- src/Tutorial/08_Text/Program.cs
+++ src/Tutorial/08_Text/Program.cs
@@ -1,15 +1,20 @@
-﻿using SdlSharp;
+﻿using System.Runtime.InteropServices;
+
+using SdlSharp;
 using SdlSharp.Graphics;
 using SdlSharp.Input;
-
 var fullscreen = false;
-using Application app = new(Subsystems.Video, fontSupport: true);
+
+using Application app =
+    (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) && Environment.OSVersion.Version.Major >= 10) ?// windows 10 及以上 使用 dx11
+    new(Subsystems.Video, fontSupport: true, hints: new[] { (Hint.RenderDriver, "direct3d11") }) :
+    new(Subsystems.Video, fontSupport: true);
 Size windowSize = new(640, 480);
 Rectangle windowRectangle = new(Window.UndefinedWindowLocation, windowSize);
 using var window = Window.Create("Text", windowRectangle, WindowOptions.Shown);
 using var renderer = Renderer.Create(window, -1, RendererOptions.Accelerated);
 using var font = Font.Create("simsun.ttf", 14);
-using var textTexture = font.RenderSolid("中文중국の城門The quick brown fox jumped over the lazy dog", Colors.White, renderer);
+using var textTexture = font.RenderSolid("图形API：" + renderer.Info.Name + " 中文중국の城門The quick brown fox jumped over the lazy dog", Colors.White, renderer);
 
 Color textColor = new(0xFF, 0xFF, 0xFF, 0xFF);
 var inputText = "Some editable text";
